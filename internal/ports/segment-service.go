5c7620ee4a3815ab5dd0cc7e2b8f3bdb3d6e6acd
Olga Shishkina <osshishkina@edu.hse.ru>
2023-08-31 16:32:08 +0300

    internal/ports: removed context from interface declaration

--- internal/ports/segment-service.go
+++ internal/ports/segment-service.go
@@ -2,19 +2,16 @@
 package ports
 
 import (
-	"context"
-	"encoding/csv"
 	"segmentation-service/internal/domain/models"
 
 	"github.com/google/uuid"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=SegmentService
 type SegmentService interface {
-	CreateSegment(ctx context.Context, slug string) error
-	DeleteSegment(ctx context.Context, slug string) error
-	UpdateUserSegments(ctx context.Context, data models.UpdateRequest, userID uuid.UUID) error
-	GetUserSegments(ctx context.Context, userID uuid.UUID) (models.SegmentsList, error)
-	GetReport(ctx context.Context, period string, wr *csv.Writer) error
-	GetUserReport(ctx context.Context, period string, userID uuid.UUID, wr *csv.Writer) error
+	CreateSegment(slug string) error
+	DeleteSegment(slug string) error
+	UpdateUserSegments(data models.UpdateRequest, userID uuid.UUID) error
+	GetUserSegments(userID uuid.UUID) (models.SegmentsList, error)
+	GetReport(period string) ([][]string, error)
+	GetUserReport(period string, userID uuid.UUID) ([][]string, error)
 }
--- internal/ports/segment-storage.go
+++ internal/ports/segment-storage.go
@@ -1,19 +1,17 @@
 package ports
 
 import (
-	"context"
 	"segmentation-service/internal/domain/models"
 
 	"github.com/google/uuid"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=SegmentStorage
 type SegmentStorage interface {
-	FindSegment(ctx context.Context, slug string) (int, error)
-	SaveSegment(ctx context.Context, slug string) error
-	DeleteSegment(ctx context.Context, slug string) error
-	UpdateUserSegments(ctx context.Context, data models.UpdateRequest, userID uuid.UUID) error
-	GetUserSegments(ctx context.Context, userID uuid.UUID) (models.SegmentsList, error)
-	GetReport(ctx context.Context, period string) ([][]string, error)
-	GetUserReport(ctx context.Context, period string, userID uuid.UUID) ([][]string, error)
+	FindSegment(slug string) (int, error)
+	SaveSegment(slug string) error
+	DeleteSegment(slug string) error
+	UpdateUserSegments(data models.UpdateRequest, userID uuid.UUID) error
+	GetUserSegments(userID uuid.UUID) (models.SegmentsList, error)
+	GetReport(period string) ([][]string, error)
+	GetUserReport(period string, userID uuid.UUID) ([][]string, error)
 }
